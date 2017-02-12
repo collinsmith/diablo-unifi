ce8e5b5e9dfdebf2d517f94d6d715029bb893a64
Collin Smith <collinsmith70@gmail.com>
2017-02-12 00:09:20 -0800

    Cleaned up CvarSuggester

--- core/src/com/gmail/collinsmith70/libgdx/CvarValueSuggester.java
+++ core/src/com/gmail/collinsmith70/libgdx/CvarValueSuggester.java
@@ -1,16 +1,15 @@
 package com.gmail.collinsmith70.libgdx;
 
-import com.google.common.base.Strings;
-
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 
 import com.gmail.collinsmith70.command.ParameterException;
 import com.gmail.collinsmith70.cvar.Cvar;
 import com.gmail.collinsmith70.diablo.Diablo;
+import com.gmail.collinsmith70.libgdx.util.ConsoleUtils;
+import com.gmail.collinsmith70.util.StringUtils;
 
 import java.util.Collection;
-import java.util.Iterator;
 
 public enum CvarValueSuggester implements Console.SuggestionProvider {
   INSTANCE;
@@ -18,61 +17,29 @@ public enum CvarValueSuggester implements Console.SuggestionProvider {
   @Override
   public int suggest(@NonNull Console console, @NonNull CharSequence buffer,
                      @NonNull String[] args, @IntRange(from = 0) int targetArg) {
-    if (buffer.length() == 0) {
-      return 0;
-    }/* else if (buffer.charAt(buffer.length() - 1) == ' ') {
-      return 0;
-    }*/
-
     String alias = args[targetArg - 1];
     Cvar cvar = Diablo.client.cvars().get(alias);
     if (cvar == null) {
-      throw new ParameterException("A parameter of type %s must precede a CvarValueSuggester",
+      throw new ParameterException(
+          "A parameter of type %s must precede a parameter using CvarValueSuggester",
           Cvar.class.getName());
     }
 
-    String str = targetArg == args.length ? "" : args[targetArg];
-    Collection<String> suggestions = cvar.suggest(str);
+    String arg = targetArg == args.length ? "" : args[targetArg];
+    Collection<String> suggestions = cvar.suggest(arg);
     switch (suggestions.size()) {
       case 0:
         return 0;
       case 1:
-        String onlySuggestion = suggestions.iterator().next();
-        console.buffer.append(onlySuggestion, str.length());
+        String suggestion = suggestions.iterator().next();
+        console.buffer.append(suggestion, arg.length());
         return 1;
       default:
-        String commonPrefix = null;
-        for (String suggestion : suggestions) {
-          if (commonPrefix == null) {
-            commonPrefix = suggestion;
-          } else if (commonPrefix.isEmpty()) {
-            break;
-          } else {
-            commonPrefix = Strings.commonPrefix(commonPrefix, suggestion);
-          }
-        }
-
-        if (commonPrefix != null && commonPrefix.length() > str.length()) {
-          console.buffer.append(commonPrefix, str.length());
+        String commonPrefix = StringUtils.commonPrefix(suggestions);
+        if (commonPrefix.length() > arg.length()) {
+          console.buffer.append(commonPrefix, arg.length());
         } else {
-          int i = 0;
-          StringBuilder sb = new StringBuilder(64);
-          for (Iterator<String> it = suggestions.iterator(); it.hasNext(); ) {
-            String suggestion = it.next();
-            if (++i % 4 == 0) {
-              sb.append(suggestion);
-              console.println(sb.toString());
-              sb.setLength(0);
-            } else if (it.hasNext()) {
-              sb.append(Strings.padEnd(suggestion, 32, ' '));
-            } else {
-              sb.append(suggestion);
-            }
-          }
-
-          if (sb.length() > 0) {
-            console.println(sb.toString());
-          }
+          ConsoleUtils.printList(console, suggestions, 4, 32);
         }
 
         return suggestions.size();
